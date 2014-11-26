a62b5a78feb8a2ce49a471ec2bd40b76b922c731
tongtien <tiennmuet@gmail.com>
2014-11-26 23:29:34 +0700

    Post Cau hoi

---
+++ protected/modules/CauHoi/components/views/index_post_hen_gio.php
@@ -0,0 +1,8 @@
+<div class="col-sm-6">
+    <select>
+        <option value="0">Thời gian thách đố...</option>
+        <?php foreach($model as $row_time):?>
+	       <option value="<?php echo $row_time->gia_tri;?>"><?php echo $row_time->ten;?></option>
+        <?php endforeach;?>
+    </select>
+</div>
