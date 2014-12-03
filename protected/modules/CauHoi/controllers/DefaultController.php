d10ff78b165de725955512e0af080eca916f8413
anhntk54 <anhntk54@gmail.com>
2014-12-04 01:42:22 +0700

    a

--- protected/modules/CauHoi/controllers/DefaultController.php
+++ protected/modules/CauHoi/controllers/DefaultController.php
@@ -2,32 +2,38 @@
 
 class DefaultController extends Controller
 {
-	public function actionIndex()
-	{
-	   
-	   $this->render('index');
-	}
+    public function actionIndex()
+    {
+       
+       $this->render('index');
+    }
     
     public function actionCreate(){
         $model = new Cauhoi;
-        if(isset($_POST['noidung'])){
-            
-            
+        if(isset($_POST['noidung']) and isset($_POST['hengio'])){
             $noidung = $_POST['noidung'];
+            $hengio = $_POST['hengio'];
+            $nowdate = date("Y-m-d H:i:s");
+            $date_hengio = date('Y-m-d H:i:s', strtotime($nowdate . ' + '.$hengio.' hours'));
+            
+            $tiencuoc = 0;
+            if(isset($_POST['tien'])){
+                $tiencuoc = $_POST['tien'];
+            }
             $file = "";
             if(isset($_POST['image']) && isset($_POST['loai'])){
                 $type_image = $_POST['loai'];
                 $img = $_POST['image'];
                 
                 if($type_image == 'jpg' || $type_image == 'jpeg'){
-    				$img = str_replace('data:image/jpeg;base64', '', $img);
-    			}
-    			if($type_image == 'png' || $type_image == 'PNG'){
-    				$img = str_replace('data:image/png;base64', '', $img);
-    			}
-    			if($type_image == 'gif' || $type_image == 'GIF'){
-    				$img = str_replace('data:image/gif;base64', '', $img);
-    			}
+                    $img = str_replace('data:image/jpeg;base64', '', $img);
+                }
+                if($type_image == 'png' || $type_image == 'PNG'){
+                    $img = str_replace('data:image/png;base64', '', $img);
+                }
+                if($type_image == 'gif' || $type_image == 'GIF'){
+                    $img = str_replace('data:image/gif;base64', '', $img);
+                }
                 
                 $img = str_replace(' ', '+', $img);
                 $_data = base64_decode($img);
@@ -44,6 +50,8 @@ class DefaultController extends Controller
             $model->user_id = Yii::app()->user->id;
             $model->noi_dung = $noidung;
             $model->anh_minhhoa = $file;
+            $model->tien_cuoc = $tiencuoc;
+            $model->hen_gio = $date_hengio;
             $model->save(false);
             // by trieu nhu
             if (isset($_POST['users'])) {
