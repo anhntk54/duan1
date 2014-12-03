98eb13015e19d48271c1e7de8b23406c97a6486c
anhntk54 <anhntk54@gmail.com>
2014-12-04 00:14:03 +0700

    tra loi

--- protected/modules/CauHoi/components/Index_Ban_Thach_Do.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Index_Ban_Thach_Do extends CWidget{
-    	public $model;
-        public function init(){
-            $this->render('index_ban_thach_do',array('model'=>$this->model));
-        }
-    }
-?>
--- protected/modules/CauHoi/components/Index_Thach_Do_Chua_Tra_Loi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Index_Thach_Do_Chua_Tra_Loi extends CWidget{
-    	public $model;
-        public function init(){
-            $this->render('index_thach_do_chua_tra_loi',array('model'=>$this->model));
-        }
-    }
-?>
