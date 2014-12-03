84c1e63ef2908013b8295fb4f33a91011dd8c1dc
anhntk54 <anhntk54@gmail.com>
2014-12-04 00:16:57 +0700

    eo dc

--- protected/modules/CauHoi/components/Index_Ban_Tra_Loi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Index_Ban_Tra_Loi extends CWidget{
-    	public $model;
-        public function init(){
-            $this->render('index_ban_tra_loi',array('model'=>$this->model));
-        }
-    }
-?>
--- protected/modules/CauHoi/views/traloi/create.php
+++
@@ -1,3 +0,0 @@
-<?php 
-	$this->widget("CauHoi.components.One_Cau_Tra_Loi",array('value'=>$value));
-?>
