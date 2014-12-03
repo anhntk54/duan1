fa79f0260ae2ddaf61bff431d483d2fd8658486a
anhntk54 <anhntk54@gmail.com>
2014-12-04 00:12:57 +0700

    content

---
+++ protected/components/views/content_right.php
@@ -0,0 +1,4 @@
+<?php $this->widget('TaiKhoan.components.Index_ThongTin_NguoiDung'); ?>
+<?php $this->widget('CauHoi.components.Index_Thach_Do_Chua_Tra_Loi'); ?>
+<?php $this->widget('CauHoi.components.Index_Ban_Thach_Do'); ?>
+<?php $this->widget('CauHoi.components.Index_Ban_Tra_Loi'); ?>
